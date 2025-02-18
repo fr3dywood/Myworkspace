0695c4ee38d2d39d451c455983b9c91c16b2e3f8
fr3dywood <frederic.clerc.fc@gmail.com>
2025-02-18 23:15:42 +0100

    BOUCLE

---
+++ Warmup/dsiplay_message.py
@@ -0,0 +1,13 @@
+"""
+Exercice : Afficher un message
+Écris un programme Python qui demande à l’utilisateur son nom, puis affiche un message de bienvenue en utilisant ce nom.
+
+Exemple du rendu attendu :
+
+Quel est ton nom ? Alice
+Bonjour, Alice ! Bienvenue en Python.
+
+"""
+
+name = input("Quel est ton nom ? ")
+print(f"Bonjour, {name} ! Bienvenue en Python.")
