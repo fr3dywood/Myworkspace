9c6c74bcc69c5255cdf9d90fabc8dbbd1d320830
fr3dywood <frederic.clerc.fc@gmail.com>
2025-02-18 22:10:37 +0100

    BOUCLE

--- 1_premier_programme/premier_programme5/main.py
+++ 1_premier_programme/premier_programme5/main.py
@@ -1,8 +1,12 @@
-"""Premier programme
-Formation Python
-apprendre la programmation"""
 
+# Demande à l'utilisateur de saisir son nom
 nom = input("Quel est votre nom ? ")
+
+# Demande à l'utilisateur de saisir son âge
 age = input("Quel est votre age ? ")
+
+# Affiche le nom et l'âge de l'utilisateur
 print("Vous vous appelez " + nom + ", vous avez " + age + " ans")
+
+# Cette ligne est commentée et ne sera pas exécutée
 # print("Merci, passez une bonne journée")
