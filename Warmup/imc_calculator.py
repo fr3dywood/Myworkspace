2544f501bdc922812be6c9c6967bc8ac5a1c78dc
fr3dywood <frederic.clerc.fc@gmail.com>
2025-02-18 22:25:51 +0100

    BOUCLE

---
+++ Warmup/imc_calculator.py
@@ -0,0 +1,33 @@
+"""
+Exercice : Calculateur d'IMC (Indice de Masse Corporelle)
+Objectif : Écrire un programme qui demande à l'utilisateur son poids et sa taille, puis calcule et affiche son IMC.
+
+Instructions :
+1) Demander à l'utilisateur son nom.
+
+2) Demander son poids en kilogrammes (float).
+
+3) Demander sa taille en mètres (float).
+
+4) Vérifier que l'utilisateur entre bien des nombres pour le poids et la taille.
+
+5) Calculer l'IMC avec la formule :
+    On prend le poids en kilogrammes (kg)
+    On prend la taille en mètres (m)
+    On multiplie la taille par elle-même (taille²)
+    On divise le poids par ce résultat
+    
+6) Afficher l'IMC avec une interprétation du résultat selon ces critères :
+    IMC < 18.5 : "Vous êtes en sous-poids."
+    18.5 ≤ IMC < 24.9 : "Votre poids est normal."
+    25 ≤ IMC < 29.9 : "Vous êtes en surpoids."
+    IMC ≥ 30 : "Vous êtes en obésité.
+
+EXEMPLE DU RESULTAT ATTENDU :
+
+    Quel est votre nom ? Alice  
+    Quel est votre poids en kg ? 65  
+    Quelle est votre taille en mètres ? 1.70  
+    Alice, votre IMC est de 22.49. Votre poids est normal.
+    """
+
