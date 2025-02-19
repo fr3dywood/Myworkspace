2ead170cfab43c9fa24c26b9ef821a95fe9fe355
fr3dywood <frederic.clerc.fc@gmail.com>
2025-02-19 10:52:24 +0100

    new

--- 1_premier_programme/premier_programme3/main.py
+++
@@ -1,6 +0,0 @@
-# Utilise la fonction input pour demander à l'utilisateur son nom et stocke la réponse dans la variable 'nom'
-nom = input("Quel est votre nom ? ")
-
-# La fonction print affiche du texte à l'écran. Ici, elle affiche "Vous vous appelez " suivi du nom entré par l'utilisateur.
-print("Vous vous appelez " + nom)
-
