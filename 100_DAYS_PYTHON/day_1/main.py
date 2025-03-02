9b9a09804e76bb8e116e7078a6a9df32fcd4fd28
System Administrator <root@MacBook-Pro-de-Frederic.local>
2025-03-02 21:27:13 +0100

    first commit

---
+++ 100_DAYS_PYTHON/day_1/main.py
@@ -0,0 +1,18 @@
+# # Demande à l'utilisateur de saisir son nom
+# nom = input("Entrez votre nom : ")
+
+# # Affiche le nom saisi
+# print(nom)
+
+# # Calcule la longueur du nom
+# longueur = len(nom)
+
+# # Affiche la longueur du nom avec un message explicatif
+# print("Le nom " + nom + " a " + str(longueur) + " caractères")
+
+print("Bienvenue dans le générateur de noms de groupe.")
+ville = input("Dans quelle ville avez-vous grandi ?")
+print(ville)
+animal = input("Quel est le nom de votre animal de compagnie ?")
+print(animal)
+print("Le nom de votre groupe pourrait être " + ville + " " + animal)
