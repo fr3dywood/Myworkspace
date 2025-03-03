79f0b939269fa8e42e07335da85ec679d8b53f2b
fr3dywood <frederic.clerc.fc@gmail.com>
2025-03-03 21:58:26 +0100

    Update

--- 100_DAYS_OF_CODE_PYTHON/day_2/calculator.py
+++ 100_DAYS_OF_CODE_PYTHON/day_2/calculator.py
@@ -1,16 +1,31 @@
-print("Bienvenue dans le calculateur de pourboires !")  # Affiche un message de bienvenue
-montant_total = (input("Quel était le montant total de la facture ?"))  # Demande le montant total de la facture
-pourboire = (input("Combien de pourboire souhaitez-vous donner ? 10, 12 ou 15 ? "))  # Demande le pourcentage de pourboire
-personnes = (input("Combien de personnes pour partager la facture ? "))  # Demande le nombre de personnes pour partager la facture
+print("Bienvenue dans le calculateur de pourboire !")
 
-montant_total = float(montant_total)  # Convertit le montant total en nombre décimal
-pourboire = int(pourboire)  # Convertit le pourcentage de pourboire en entier
-personnes = int(personnes)  # Convertit le nombre de personnes en entier
+# Demande à l'utilisateur le montant total de la facture
+total = (input("Quel était le montant total de la facture ? $"))
 
-pourboire = pourboire / 100  # Convertit le pourcentage de pourboire en décimal
-pourboire = montant_total * pourboire  # Calcule le montant du pourboire
-montant_total = montant_total + pourboire  # Ajoute le pourboire au montant total
-montant_total = montant_total / personnes  # Divise le montant total par le nombre de personnes
-montant_total = round(montant_total, 2)  # Arrondit le montant à deux décimales
+# Demande à l'utilisateur le pourcentage de pourboire qu'il souhaite donner
+pourboire = (input("Combien de pourboire souhaitez-vous donner ? 10, 12 ou 15 ? "))
 
-print(f"Chaque personne doit payer : {montant_total}")  # Affiche le montant que chaque personne doit payer
+# Demande à l'utilisateur combien de personnes vont partager la facture
+personnes = (input("Combien de personnes pour partager la facture ? "))
+
+# Convertit les entrées utilisateur en types de données appropriés
+total = float(total)
+pourboire = int(pourboire)
+personnes = int(personnes)
+
+# Calcule le montant du pourboire
+pourboire = pourboire / 100
+pourboire = total * pourboire
+
+# Ajoute le pourboire au total de la facture
+total = total + pourboire
+
+# Divise le total par le nombre de personnes pour obtenir le montant par personne
+total = total / personnes
+
+# Arrondit le montant à deux décimales
+total = round(total, 2)
+
+# Affiche le montant que chaque personne doit payer
+print(f"Chaque personne doit payer : ${total}")
--- 100_DAYS_OF_CODE_PYTHON/day_3/cours.py
+++ 100_DAYS_OF_CODE_PYTHON/day_3/cours.py
@@ -1,1 +1,35 @@
-print("hello world")
+# print("Bienvenue dans la tour de la terreur")
+# taille = int(input("quelle est votre taille ? "))
+
+# if taille >= 120 :
+#     print("Vous pouvez monter")
+#     age = int(input("Quel est votre age ? "))
+#     if age <= 12 :
+#         print("Vous devez payer 5 euros")
+#     elif age <= 18 :
+#         print("Vous devez payer 7 euros")
+#     else:
+#         print("Vous devez payer 12 euros")    
+# else:    
+#     print("Vous ne pouvez pas monter")
+#     print("Merci d'avoir visité la tour de la terreur") 
+
+# # nombre = int(input("Entrez un nombre : "))
+# # if nombre % 2 == 0:
+# #     print("Le nombre est pair")
+# # else:
+# #     print("Le nombre est impair")
+# # print("Merci d'avoir utilisé mon programme")
+
+weight = int(input("Entrez votre poids en kg : "))
+height = float(input("Entrez votre taille en m : "))
+
+bmi = weight / (height ** 2)
+
+
+if bmi >= 25:
+    print("overweight")
+elif bmi >= 18.5:
+    print("normal weight")
+else:
+    print("underweight")
