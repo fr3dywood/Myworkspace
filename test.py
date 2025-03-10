405351c2f603a174a82f2fe9bd762efa1750535c
fr3dywood <frederic.clerc.fc@gmail.com>
2025-03-10 19:00:01 +0100

    Update

--- test.py
+++ test.py
@@ -1,18 +1,39 @@
+import random
+
 
 def demander_nombre(nb_min, nb_max):
-    nombre_str = input(f"Quel est le nombre magfique entre ({nb_min} et {nb_max}) ? ")
-    nombre_int = int(nombre_str)
+    nombre_int = 0
+    while nombre_int == 0:
+        nombre_str = input(f"Quel est le nombre magfique entre ({nb_min} et {nb_max}) ? ")
+        try:
+            nombre_int = int(nombre_str)
+        except:
+            print("ERREUR : Entrez un nombre. Réessayer. ")
+        else:
+            if nombre_int < nb_min or nombre_int > nb_max:
+                print(f"Erreur: Le nombre doit être en {nb_min} et {nb_max}. Réessayer.")
+                nombre_int = 0
     return nombre_int
 
 
 NOMBRE_MIN = 1
 NOMBRE_MAX = 10
-NOMBRE_MAGIQUE = 5
+NOMBRE_MAGIQUE = random.randint(NOMBRE_MIN, NOMBRE_MAX)
+NB_VIES = 4
 
-nombre = demander_nombre(NOMBRE_MIN, NOMBRE_MAX)
-if nombre == NOMBRE_MAGIQUE:
-    print("Bravo, vous avez gagné !")
-elif nombre > NOMBRE_MAGIQUE:
-    print("Le nombre magique est plus petit")
-else:
-    print("Le nombre magique est plus grand")
+nombre = 0
+vies = NB_VIES
+while not nombre == NOMBRE_MAGIQUE and vies > 0:
+    print(f"Il vous reste {vies} vies")
+    nombre = demander_nombre(NOMBRE_MIN, NOMBRE_MAX)
+    if nombre == NOMBRE_MAGIQUE:
+        print("Bravo, vous avez gagné !")
+    elif nombre > NOMBRE_MAGIQUE:
+        print("Le nombre magique est plus petit")
+        vies -= 1
+    else:
+        print("Le nombre magique est plus grand")
+        vies -= 1
+
+if vies == 0:
+    print("Vous avez perdu!")
